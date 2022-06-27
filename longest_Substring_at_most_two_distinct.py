cbb810eb2a237a5ce24925233a5fc0fa0b76776c
Nandhinee Periyakaruppan <nandhinee2010@gmail.com>
2022-06-27 17:36:22 +0530

    Added optimisation for Longest Substring with At Most Two Distinct Characters

--- longest_Substring_at_most_two_distinct.py
+++ longest_Substring_at_most_two_distinct.py
@@ -12,17 +12,30 @@ class Solution(object):
         i = 0
         res = 0
         for j, ch in enumerate(s):
-            
-            if ch in ch_count:
-                ch_count[ch] += 1
-            else:
-                ch_count[ch] = 1
-                
+            if ch in ch_count: ch_count[ch] += 1
+            else: ch_count[ch] = 1
             while self.checkSubstringForMoreThanTwoDistinct(ch_count):
                 i += 1
                 ch_count[s[i-1]] -= 1
-                if ch_count[s[i-1]] == 0:
-                    del ch_count[s[i-1]]
-                    
+                if ch_count[s[i-1]] == 0: del ch_count[s[i-1]]
             res = max(res, j-i+1)
         return res
+
+    # Optimisation
+    def lengthOfLongestSubstringTwoDistinctOptimised(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ch_count = {}
+        i = 0
+        res = 0
+        for j, ch in enumerate(s):
+            ch_count[ch] = j
+            if self.checkSubstringForMoreThanTwoDistinct(ch_count):
+                i = min(ch_count.values()) + 1
+                del ch_count[s[i-1]]
+            res = max(res, j-i+1)
+        return res
+            
+            
