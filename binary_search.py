4922d797e42751d04f193ddede0ca2076c27ccbe
Nandhinee Periyakaruppan <nandhinee2010@gmail.com>
2022-07-26 13:48:46 +0530

    Added iterative approach to binary search.py

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,7 @@
 class Solution(object):
     
+    # ========================== APPROACH 1 ==========================
+    # Recursive style
     def binary_search(self, start_index, end_index, nums, target):
         if start_index <= end_index:
             mid = (start_index + end_index)//2
@@ -22,3 +24,24 @@ class Solution(object):
         result = self.binary_search(0, len(nums)-1, nums, target)
         return result
 
+    # ========================== APPROACH 2 ==========================
+    # Iterative style
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while (left <= right):
+            mid = (left + right) // 2
+            if nums[mid] == target: 
+                return mid
+            if nums[mid] > target: 
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
