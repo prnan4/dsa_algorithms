33aeb8dad7977439adc159189b4f84471790f673
Nandhinee Periyakaruppan <nandhinee2010@gmail.com>
2022-09-06 00:35:41 -0400

    Added backtracking approach for letter combinations of phone number

--- letter_combinations_of_phone_number.py
+++ letter_combinations_of_phone_number.py
@@ -1,4 +1,6 @@
 class Solution(object):
+    #========================== APPROACH 1 ==========================
+    # Iterative method
     def letterCombinations(self, digits):
         """
         :type digits: str
@@ -22,6 +24,37 @@ class Solution(object):
                 print(result)
             digits = digits[1:]
         return result
+
+    #========================== APPROACH 1 ==========================
+    # Backtracking approach
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        char_dict = {2: ['a', 'b', 'c'], 3:['d', 'e', 'f'], 4:['g', 'h', 'i'], 5:['j', 'k', 'l'], 6:['m', 'n', 'o'], 7:['p', 'q', 'r', 's'], 8:['t', 'u', 'v'], 9:['w', 'x', 'y', 'z']}
+        
+        output = []
+        
+        if not digits:
+            return output
+        
+        def backtrack(start, word):
+            if start == len(digits):
+                output.append(word)
+                return
+            n = len(char_dict[int(digits[start])])
+            
+            for i in range(0, n):
+                word += char_dict[int(digits[start])][i]
+                backtrack(start +1, word)
+                word = word[:-1]
+            
+        backtrack(0, "")
+        return output
+    
+        
+        
                         
             
         
